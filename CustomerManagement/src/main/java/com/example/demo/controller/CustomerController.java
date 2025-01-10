c5b4d833fae65595f7aad845473e7f2802c0317d
Ankit Bopche <ankitbop999@gmail.com>
2025-01-10 09:46:27 +0530

    initial code

---
+++ CustomerManagement/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+	
+	
+	
+//	public void addCustomer(@RequestBody Customer customer)
+
+	
+	public void m1() {
+		System.out.println("new");
+	}
+
+}
